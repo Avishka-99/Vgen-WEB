f7ba4e51a1877fcdd87400eece16a0567dd545c6
Avishka Prabhath <avishkaprabha360@gmail.com>
2023-07-29 19:19:38 +0530

    deleted unwanted files

--- client/src/routes/Routes.js
+++ client/src/routes/Routes.js
@@ -1,16 +1,4 @@
 import * as UserViews from "../constants/AllViews";
-// import Home from '../views/customer/Home'
-// import Order from "../views/customer/Order";
-// import Categories from "../views/customer/Categories";
-// import Community from "../views/customer/Community";
-// import Feed from "../views/customer/Feed";
-// import Restaurants from "../views/customer/Restaurants";
-// import RestaurantHome from "../views/restaurant/RestaurantHome";
-// import RestaurantProducts from "../views/restaurant/RestaurantProducts";
-// import OrdersView from "../views/restaurant/OrdersView";
-// import Reservation from "../views/restaurant/Reservation";
-// import Shopping from "../views/restaurant/Shopping";
-// import RestaurantProductAdd from "../views/restaurant/RestaurantProductAdd";
 export const customerRoutes = [
   { id: 1, path: "/home", element: <UserViews.Home /> },
   { id: 2, path: "/category", element: <UserViews.Categories/> },
