fdcf72a5e471def2bd89059170b04ae5378b3756
Avishka Prabhath <avishkaprabha360@gmail.com>
2023-07-29 22:04:35 +0530

    admin windows

--- client/src/components/MenuItem.js
+++ client/src/components/MenuItem.js
@@ -3,7 +3,7 @@ import MenuItemIcon from "./MenuItemIcon";
 export default function MenuItem(props) {
   return (
     <div className="menuItem" onClick={() => props.fun(props.link, props.index)} tabIndex={props.index}>
-      <MenuItemIcon icon={props.icon} color={props.active == props.index ? "#FB9300" : "#6F767F"} className="menuIcon" />
+      <MenuItemIcon icon={props.icon} color={props.active == props.index ? "#03C988" : "#6F767F"} className="menuIcon" />
     </div>
   );
 }
