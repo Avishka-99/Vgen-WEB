18a14e77da986f309b54b02167a1b9f568395870
Avishka Prabhath <avishkaprabha360@gmail.com>
2023-07-29 20:36:20 +0530

    sidebar css changed

--- client/src/components/MenuItem.js
+++ client/src/components/MenuItem.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import MenuItemIcon from './MenuItemIcon'
+import React from "react";
+import MenuItemIcon from "./MenuItemIcon";
 export default function MenuItem(props) {
   return (
-    <div className='menuItem' onClick={() => props.fun(props.link)} tabIndex={props.index}>
-      <MenuItemIcon icon={props.icon} className="menuIcon" />
+    <div className="menuItem" onClick={() => props.fun(props.link, props.index)} tabIndex={props.index}>
+      <MenuItemIcon icon={props.icon} color={props.active == props.index ? "#FB9300" : "#6F767F"} className="menuIcon" />
     </div>
-  )
+  );
 }
