08bd726bc64ca90cf8573817f297b8c5655fa14e
Avishka Prabhath <avishkaprabha360@gmail.com>
2023-08-05 08:26:26 +0530

    sidebar fixed

--- client/src/components/MenuItem.js
+++ client/src/components/MenuItem.js
@@ -7,13 +7,14 @@ const MenuItem = (props) => {
       className={`menuItem ${props.active === props.index ? 'active' : ''}`}
       onClick={() => props.fun(props.link, props.index)}
       tabIndex={props.index}
+      style={props.style}
     >
       <MenuItemIcon
         icon={props.icon}
         color={props.active === props.index ? "#03C988" : "#6F767F"}
         className="menuIcon"
       />
-      {props.label && <span className="menuLabel">{props.label}</span>}
+      {props.label && <span className="menuLabel" style={props.labelMargin}>{props.label}</span>}
     </div>
   );
 };
