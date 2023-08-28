8ff55505dfe96bc5fd743e93a94489572a8e6a5b
Avishka Prabhath <avishkaprabha360@gmail.com>
2023-08-28 07:14:46 +0530

    restaurant

--- client/src/views/restaurant/RestaurantItem .js
+++ client/src/views/restaurant/RestaurantItem .js
@@ -1,33 +1,34 @@
-import React from "react";
+import React from 'react';
 
+const RestaurantItem = ({data, oneProductHandle}) => {
+	//console.log(data);
+	const handleClick = () => {
+		oneProductHandle(data);
+	};
+	//console.log(data.products[0]);
 
-const RestaurantItem = ({data,oneProductHandle}) => {
-  console.log(data);
-  const handleClick = () => {
-    oneProductHandle(data);
-  };
-
-
-    return (  
-
-      <div className="outer-frame">
-          <div className="frame-up">
-          <img src={`http://localhost:5001/uploads/products/${data.products[0].productImage}`} alt="product" />
-          </div>
-          <div className="frame-down">
-            <h5>{data.products[0].productName}</h5>
-            {/* <label htmlFor="">Product Name: </label><br /> */}
-            {/* <span>{data.products[0].productName}</span><br /> */}
-            {/* <label htmlFor="">Product quantity: </label><br />
+	return (
+		<div className='outer-frame'>
+			<div className='frame-up'>
+				<img src={`http://localhost:5001/uploads/products/${data.products[0].productImage}`} alt='product' />
+			</div>
+			<div className='frame-down'>
+				<h5>{data.products[0].productName}</h5>
+				{/* <label htmlFor="">Product Name: </label><br /> */}
+				{/* <span>{data.products[0].productName}</span><br /> */}
+				{/* <label htmlFor="">Product quantity: </label><br />
             <span>{data.quantity}</span><br />
             <label htmlFor="">Product price: </label><br />
             <span>Rs .{data.price}</span><br /> */}
-            {/* <p>This is {data.products[0].description}</p> */}
-            <button className="frame-update-btn" onClick={handleClick}>update</button><br />
-            <button className="frame-delete-btn" >delete</button>
-          </div>
-      </div>
-    );
-}
- 
+				{/* <p>This is {data.products[0].description}</p> */}
+				<button className='frame-update-btn' onClick={handleClick}>
+					update
+				</button>
+				<br />
+				<button className='frame-delete-btn'>delete</button>
+			</div>
+		</div>
+	);
+};
+
 export default RestaurantItem;
