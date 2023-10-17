bb68a0acce2beee8d3e182458d4c6122a581a681
Daweendri <83944867+Daweendri@users.noreply.github.com>
2023-10-17 11:48:55 +0530

    changes

---
+++ client/src/keys/Keys.js
@@ -0,0 +1,2 @@
+export const STRIPE_PUBLIC_KEY = 'pk_test_51Ne9coLBjUooMXI146teBtEm8hba5O7sWqwdAa9g5QFxo8LST6RlALictlucEo8YePm79OJTDkKWnxm1Z9fuVodW00RLh6Kwdz';
+export const GOOGLE_API = 'AIzaSyDGf0EXb4I0BQoE2t_IsJmkOJXYTc0S5bA';
