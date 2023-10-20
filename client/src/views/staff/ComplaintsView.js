7a9a5baeab296b6c69440664c81c7419e5eb06f5
Dewmini Devindya <120557607+dewminidevindya@users.noreply.github.com>
2023-10-20 05:39:30 +0530

    ComplaintsView

---
+++ client/src/views/staff/ComplaintsView.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../../styles/staff/staffhome.css";
+import "../../styles/staff/staffpayments.css";
+import "../../styles/staff/staffcomplaints.css";
+
+export default function Complains() {
+  return (
+    <div className="AdminContainer">
+      
+    </div>
+  )
+}
